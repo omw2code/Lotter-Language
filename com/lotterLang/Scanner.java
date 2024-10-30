b106b07fa084f68acccb12b7c4f2d3e3fe187aae
Brennan Rivera <brennanrivera0@gmail.com>
2024-10-29 21:50:03 -0400

    Continued to build out the lexer

--- com/lotterLang/Scanner.java
+++ com/lotterLang/Scanner.java
@@ -7,19 +7,23 @@ import java.util.Map;
 
 import static com.lotterLang.TokenType.*;
 
-class Scanner {
+class Scanner 
+{
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;
     private int current = 0;
     private int line = 1;
 
-    Scanner(String source) {
+    Scanner(String source) 
+    {
         this.source = source;
     }
 
-    List<Token> scanTokens() {
-        while (current < source.length()) {
+    List<Token> scanTokens() 
+    {
+        while (current < source.length()) 
+        {
             // beginning of the next lexeme
             start = current;
             scanToken();
@@ -28,7 +32,8 @@ class Scanner {
         return tokens;
     }
 
-    private void scanToken() {
+    private void scanToken() 
+    {
         char c = advance();
         switch (c) {
             case '(': addToken(LEFT_PAREN); break;
@@ -41,27 +46,80 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(ASTERISK); break;
-
+            // cases where we read a follow up character for operators
+            case '!': 
+                addToken(match('=') ? EXCLA_EQUAL : EXCLA);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '>':
+                addToken(match('=') ? LESS_EQUAL : EQUAL);
+                break;
+            case '/':
+                // NOTE: Look into optimizing this peek approach
+                if (match('/')) // if we match a comment we want to finish iterating the line
+                {
+                    while(peek() != '\n' && !isAtEnd()) advance();
+                }
+                else 
+                {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':  
+            case '\r': 
+            case '\t': 
+                break;
+            case '\n': 
+                line++;
+                break;
             default:
                 Lotter.error(line, "Unexpected character.");
                 break;
         }
     }
-    // incriment and return the corresponding character
-    private char advance() {
+    // incriment and return the prev character.
+    // this will be useful in edge cases that require follow up characters
+    private char advance() 
+    {
         current++;
         return source.charAt(current - 1);
     }
 
-    private void addToken(TokenType type) {
+    private void addToken(TokenType type) 
+    {
         addToken(type, null);
     }
 
-    private void addToken(TokenType type, Object literal) {
+    private void addToken(TokenType type, Object literal) 
+    {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
 
+    private boolean match(char expected) 
+    {
+        if (isAtEnd()) return false; 
+        if (source.charAt(current) != expected) return false;
+        
+        current++;
+        return true;
+    }
 
+    // this allows us to look ahead at the current unconsumed character
+    private char peek()
+    {
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
 
+    private boolean isAtEnd()
+    {
+        //implement this
+        return false;
+    }
 }
